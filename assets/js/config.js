888916cea4ef1254661a3ef199af8181a39ae983
Rima Bhumbla <105192284+rbhumbla1@users.noreply.github.com>
2022-12-01 13:32:00 -0800

    edited some comments

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
-//Ignore this file - it got upload to github even when it was in github and then got corrupted when we tried to remove it.
-//This file will be added to .gitignore
+//This file will be added to .gitignore - which is not working right now with proper env setup 
+//but will still use it to learn about export/import
 
 //apikey for openweathermap API access 
 
-export const apiKey = ""; 
+export const apiKey = "b0bb308922887642a5d7fe5054695311"; 
